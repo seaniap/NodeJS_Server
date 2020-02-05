df114eb4157482529b191001483d5660164106af
SeanYeh <sean@ai-ad.com.tw>
2020-02-05 14:47:04 +0800

    if statement added

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ let http = require('http');
 
 let myApp = http.createServer(function (req, res) {
     res.writeHead(200, {
-        "Content-Type": "text/html"
+        "Content-Type": "text/html; charset=utf-8"
     })
-    res.end("Welcome");
+    if (req.url == '/') {
+        res.end("歡迎來到首頁。")
+    } else if (req.url == '/about') {
+        res.end("歡迎來到關於我們頁")
+    } else {
+        res.end("抱歉，找不到您的網頁。")
+    }
 });
 
 myApp.listen(3000);
