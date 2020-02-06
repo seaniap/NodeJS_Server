18124e85ca0db4570f2035d04b09e508a565dc2e
SeanYeh <sean@ai-ad.com.tw>
2020-02-06 18:10:21 +0800

    add comments

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ let fs = require('fs');
 let url = require('url');
 
 let myApp = http.createServer(function (req, res) {
+    //取得輸入的網址
     let q = url.parse(req.url, true);
+    //取得輸入的網址名稱
     console.log(q.pathname);
     let filename = "." + q.pathname;
+    // 首頁預設
+    if (filename == './') {
+        filename = './index.html'
+    }
     fs.readFile(filename, function (err, data) {
+        //判斷錯誤
         if (err) {
             res.writeHead(404, {
                 "Content-Type": "text/html; charset=utf-8"
             });
             return res.end("404 Not Found");
         }
+        //如果沒有錯誤時。
         res.writeHead(200, {
             "Content-Type": "text/html; charset=utf-8"
         });
